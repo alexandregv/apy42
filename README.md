023d2f1b4fd6f50e97767b9d4ed356e7c8d0009e
reach <reach@42paris.fr>
2020-07-31 10:34:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python 42 api connector
+
+# Installation
+
+1) `git clone https://github.com/alexandregv/42apy`
+2) `pip install -r requirements.txt`
+
+# Usage
+
+- With arguments: `python a.py /users/reach`
+- Interactively: `python a.py` then type `/users/reach`
---
+++ a.py
@@ -0,0 +1,24 @@
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+from pygments import highlight, lexers, formatters
+
+import sys
+import json
+import os
+
+client_id = os.environ['API42_ID']
+client_secret = os.environ['API42_SECRET']
+
+client = BackendApplicationClient(client_id=client_id)
+api = OAuth2Session(client=client)
+token = api.fetch_token(token_url='https://api.intra.42.fr/oauth/token', client_id=client_id, client_secret=client_secret)
+
+if len(sys.argv) <= 1:
+    endpoint = raw_input()
+else:
+    endpoint = sys.argv[1]
+
+response = api.get('https://api.intra.42.fr/v2/' + endpoint)
+raw_json = response.content
+colored_json = highlight(json.dumps(json.loads(raw_json), indent=2, ensure_ascii=False), lexers.JsonLexer(), formatters.TerminalFormatter())
+print(colored_json)
