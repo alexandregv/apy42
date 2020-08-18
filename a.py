6d015ce638ccacd3fe8f4b0e2a9e9a2afe6bf2c5
reach <reach@42paris.fr>
2020-08-18 20:37:00 +0200

    Loop when interactive mode + color prompt

--- a.py
+++ a.py
@@ -7,31 +7,46 @@ from pygments import highlight, lexers, formatters
 import sys
 import json
 import os
+import time
 import readline
+from builtins import input
 
-client_id = os.environ['API42_ID']
-client_secret = os.environ['API42_SECRET']
+def call(api, endpoint):
+    response = api.get('https://api.intra.42.fr/v2/' + endpoint)
+    raw_json = response.content
 
-client = BackendApplicationClient(client_id=client_id)
-api = OAuth2Session(client=client)
-token = api.fetch_token(token_url='https://api.intra.42.fr/oauth/token', client_id=client_id, client_secret=client_secret)
+    if sys.stdout.isatty():
+        colored_json = highlight(json.dumps(json.loads(raw_json), indent=2, ensure_ascii=False), lexers.JsonLexer(), formatters.TerminalFormatter())
+        print(colored_json)
+    else:
+        print(raw_json.decode('utf-8'))
 
-tty = sys.stdout.isatty()
 
-if len(sys.argv) <= 1:
+def prompt(api):
+    if len(sys.argv) <= 1:
+        if sys.stdout.isatty():
+            sys.stderr.write(str("\033[91m> https://api.intra.42.fr/v2/\033[0m"))
+        endpoint = input()
+        call(api, endpoint)
+        prompt(api)
+    else:
+        endpoint = sys.argv[1]
+        call(api, endpoint)
+
+
+def main():
+    client_id = os.environ['API42_ID']
+    client_secret = os.environ['API42_SECRET']
+
+    client = BackendApplicationClient(client_id=client_id)
+    api = OAuth2Session(client=client)
+    token = api.fetch_token(token_url='https://api.intra.42.fr/oauth/token', client_id=client_id, client_secret=client_secret)
+
+    prompt(api)
+
+
+if __name__ == "__main__":
     try:
-        input = raw_input
-    except NameError:
-        pass
-    endpoint = input("> https://api.intra.42.fr/v2/" if tty else "")
-else:
-    endpoint = sys.argv[1]
-
-response = api.get('https://api.intra.42.fr/v2/' + endpoint)
-raw_json = response.content
-
-if tty:
-    colored_json = highlight(json.dumps(json.loads(raw_json), indent=2, ensure_ascii=False), lexers.JsonLexer(), formatters.TerminalFormatter())
-    print(colored_json)
-else:
-    print(raw_json.decode('utf-8'))
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
