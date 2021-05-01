fd1d175628bfce62f102b02b4d04d180f95401f2
Reach <reach@42paris.fr>
2021-05-01 03:14:59 +0200

    WIP recursive calls

--- a.py
+++ a.py
@@ -3,6 +3,7 @@
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 from pygments import highlight, lexers, formatters
+import urllib.parse
 
 import sys
 import json
@@ -12,10 +13,68 @@ import readline
 from builtins import input
 
 
-def call(api, endpoint):
-    response = api.get('https://api.intra.42.fr/v2/' + endpoint)
-    raw_json = response.content
-    return raw_json
+
+def recursive_urlencode(d):
+    def recursion(d, base=[]):
+        pairs = []
+        for key, value in d.items():
+            new_base = base + [key]
+            if hasattr(value, 'values'):
+                pairs += recursion(value, new_base)
+            else:
+                new_pair = None
+                if len(new_base) > 1:
+                    first = new_base.pop(0)
+                    rest = map(lambda x: x, new_base)
+                    new_pair = "%s[%s]=%s" % (first, ']['.join(rest), str(value))
+                else:
+                    new_pair = "%s=%s" % (str(key), str(value))
+                pairs.append(new_pair)
+        return pairs
+    return '&'.join(recursion(d))
+
+def recursive_urlencode_quote(d):
+    def recursion(d, base=[]):
+        pairs = []
+        for key, value in d.items():
+            new_base = base + [key]
+            if hasattr(value, 'values'):
+                pairs += recursion(value, new_base)
+            else:
+                new_pair = None
+                if len(new_base) > 1:
+                    first = urllib.parse.quote(new_base.pop(0))
+                    rest = map(lambda x: urllib.parse.quote(x), new_base)
+                    new_pair = "%s[%s]=%s" % (first, ']['.join(rest), urllib.parse.quote(str(value)))
+                else:
+                    new_pair = "%s=%s" % (urllib.parse.quote(str(key)), urllib.parse.quote(str(value)))
+                pairs.append(new_pair)
+        return pairs
+    return '&'.join(recursion(d))
+
+
+
+def call(api, endpoint, params = {}):
+    response = api.get(f'https://api.intra.42.fr/v2/{endpoint}', params=recursive_urlencode(params))
+    return response
+
+
+def call_rec(api, endpoint, params = {}, page = 1):
+    params = {
+        **params,
+        "page": {
+            "size": 100,
+            "number": page,
+        },
+    }
+    response = api.get(f'https://api.intra.42.fr/v2/{endpoint}', params=recursive_urlencode(params))
+    #print(response.request.url)
+    #print(response)
+    raw_json = response.json()
+    if raw_json:
+        return raw_json + call_rec(api, endpoint, params, page + 1)
+    else:
+        return []
 
 
 def print_results(raw_json):
@@ -34,11 +93,11 @@ def prompt(api):
         if sys.stdout.isatty():
             sys.stderr.write(str("\033[91m> https://api.intra.42.fr/v2/\033[0m"))
         endpoint = input()
-        print_results(call(api, endpoint))
+        print_results(call(api, endpoint).content)
         prompt(api)
     else:
         endpoint = sys.argv[1]
-        print_results(call(api, endpoint))
+        print_results(call(api, endpoint).content)
 
 
 def init_api():
@@ -47,8 +106,7 @@ def init_api():
 
     client = BackendApplicationClient(client_id=client_id)
     api = OAuth2Session(client=client)
-    token = api.fetch_token(token_url='https://api.intra.42.fr/oauth/token', client_id=client_id, client_secret=client_secret)
-
+    token = api.fetch_token(token_url='https://api.intra.42.fr/oauth/token', client_id=client_id, client_secret=client_secret, scope=["public", "projects", "profile", "tig", "forum", "elearning"])
     return(api)
 
 
